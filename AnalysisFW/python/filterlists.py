f6ee307b0c8a506f749c7f84aedb5598d9400ad4
Hannu Siikonen <hannu.siikonen@cern.ch>
2019-06-10 09:10:25 +0200

    Updating filter lists.

--- AnalysisFW/python/filterlists.py
+++ AnalysisFW/python/filterlists.py
@@ -1,40 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
+# These should be reviewed from time to time. See the listings at:
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2
+
 fltlist = {
-  '16' : {
+  '16' : { # 8_0_X and 9_4_X MINIAOD (which originates from 8_0_X AOD) 
     'dt' : cms.vstring(
-      "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-      "Flag_goodVertices","Flag_eeBadScFilter","Flag_globalSuperTightHalo2016Filter",
-      "Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter",
+      "Flag_eeBadScFilter"
     ),
     'mc' : cms.vstring(
-      "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter",
-      "Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter"
     )
   },
-  '17' : {
+  '17' : { # 9_4_0 and further
     'dt' : cms.vstring(
-     "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-     "Flag_goodVertices","Flag_eeBadScFilter","Flag_ecalBadCalibFilter",
-     "Flag_globalSuperTightHalo2016Filter","Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter",
+      "Flag_eeBadScFilter",
+      "Flag_ecalBadCalibReducedMINIAODFilter"
     ),
     'mc' : cms.vstring(
-     "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-     "Flag_goodVertices","Flag_ecalBadCalibFilter",
-     "Flag_globalSuperTightHalo2016Filter","Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter",
+      "Flag_ecalBadCalibReducedMINIAODFilter"
     )
   },
-  '18' : {
+  '18' : { # 10_1_X and further
     'dt' : cms.vstring(
-     "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-     "Flag_goodVertices","Flag_eeBadScFilter","Flag_ecalBadCalibFilter",
-     "Flag_globalSuperTightHalo2016Filter","Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter",
+      "Flag_eeBadScFilter",
+      "Flag_ecalBadCalibReducedMINIAODFilter"
     ),
     'mc' : cms.vstring(
-     "Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter","Flag_EcalDeadCellTriggerPrimitiveFilter",
-     "Flag_goodVertices","Flag_ecalBadCalibFilter",
-     "Flag_globalSuperTightHalo2016Filter","Flag_BadChargedCandidateFilter","Flag_BadPFMuonFilter"
+      "Flag_goodVertices","Flag_globalSuperTightHalo2016Filter","Flag_HBHENoiseFilter","Flag_HBHENoiseIsoFilter",
+      "Flag_EcalDeadCellTriggerPrimitiveFilter","Flag_BadPFMuonFilter",
+      "Flag_ecalBadCalibReducedMINIAODFilter"
     )
   }
 }
